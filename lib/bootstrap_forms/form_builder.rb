9bffe60a5aac7a784628ce57399c6603bf5b304e
Henrik N <henrik@nyh.se>
2012-02-08 11:58:35 +0100

    Actually translate class and attribute names.

--- lib/bootstrap_forms/form_builder.rb
+++ lib/bootstrap_forms/form_builder.rb
@@ -6,7 +6,7 @@ module BootstrapForms
       if object.errors.full_messages.any?
         content_tag(:div, :class => 'alert alert-block alert-error') do
           link_to('&times;'.html_safe, '#', {:class => 'close', :data => { :dismiss => 'alert' }}) +
-          content_tag(:h4, I18n.t('bootstrap_forms.errors.header', :model => object.class.model_name.humanize), :class => 'alert-heading') +
+          content_tag(:h4, I18n.t('bootstrap_forms.errors.header', :model => object.class.model_name.human), :class => 'alert-heading') +
           content_tag(:ul) do
             object.errors.full_messages.map do |message|
               content_tag(:li, message)
@@ -45,7 +45,7 @@ module BootstrapForms
       control_group_div do
         input_div do
           label(@name, :class => [ 'checkbox', required_class ].compact.join(' ')) do
-            extras { super(name, *(@args << @options)) + object.class.human_attribute_name(name) }
+            extras { super(name, *(@args << @options)) + human_attribute_name }
           end
         end
       end
@@ -123,7 +123,7 @@ module BootstrapForms
 
     private
     def control_group_div(&block)
-      @options[:error] = object.errors[@name].collect{|e| "#{@options[:label] || @name} #{e}".humanize}.join(', ') unless object.errors[@name].empty?
+      @options[:error] = error_string
 
       klasses = ['control-group']
       klasses << 'error' if @options[:error]
@@ -132,6 +132,19 @@ module BootstrapForms
       klass = klasses.join(' ')
 
       content_tag(:div, :class => klass, &block)
+    end
+
+    def error_string
+      errors = object.errors[@name]
+      if errors.present?
+        errors.map { |e|
+          "#{@options[:label] || human_attribute_name} #{e}"
+        }.join(", ")
+      end
+    end
+
+    def human_attribute_name
+      object.class.human_attribute_name(@name)
     end
 
     def input_div(&block)
