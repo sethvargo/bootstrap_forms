efda09f693297e9d6899eed9000ae3fec6931e85
albertodega <albertodega@gmail.com>
2012-04-30 16:58:40 +0300

    added the possibility to pass label: '' to radio_buttons and check_box, causing the absence of labels in outputted html.

--- lib/bootstrap_forms/form_builder.rb
+++ lib/bootstrap_forms/form_builder.rb
@@ -40,8 +40,12 @@ module BootstrapForms
 
       control_group_div do
         input_div do
-          label(@name, :class => [ 'checkbox', required_class ].compact.join(' ')) do
+          if @field_options[:label] == ""
             extras { super(name, *(@args << @field_options)) + (@field_options[:label].blank? ? human_attribute_name : @field_options[:label])}
+          else
+            label(@name, :class => [ 'checkbox', required_class ].compact.join(' ')) do
+              extras { super(name, *(@args << @field_options)) + (@field_options[:label].blank? ? human_attribute_name : @field_options[:label])}
+            end
           end
         end
       end
@@ -54,8 +58,12 @@ module BootstrapForms
       control_group_div do
         label_field + input_div do
           values.map do |text, value|
-            label("#{@name}_#{value}", :class => [ 'radio', required_class ].compact.join(' ')) do
+            if @field_options[:label] == ""
               extras { radio_button(name, value, @options) + text }
+            else
+              label("#{@name}_#{value}", :class => [ 'radio', required_class ].compact.join(' ')) do
+                extras { radio_button(name, value, @options) + text }
+              end
             end
           end.join.html_safe
         end
