55de63bd9d39e14ae277fc0b1673f27d0408926b
Seth Vargo <sethvargo@gmail.com>
2012-12-11 00:56:38 -0500

    merge WGretz/master. closes #61

--- spec/dummy/app/models/project.rb
+++ spec/dummy/app/models/project.rb
@@ -2,4 +2,10 @@ class Project < ActiveRecord::Base
   has_many :tasks
   accepts_nested_attributes_for :tasks
   validates :owner, :presence => true
+  validates :if_presence, :presence => true, :if => lambda { true}
+  validates :unless_presence, :presence => true, :unless => lambda { true}
+  validates :create_presence, :presence => true, :on => :create
+  validates :update_presence, :presence => true, :on => :update
+  attr_accessor :if_presence, :unless_presence, :create_presence, :update_presence
+  
 end
