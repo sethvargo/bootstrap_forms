d2a98d1bea439e0e0f519fe50563bcda9346b12f
Michael Witrant <mike@lepton.fr>
2013-02-24 16:18:46 +0100

    converted tabs to spaces

--- spec/dummy/app/helpers/my_custom_form_builder.rb
+++ spec/dummy/app/helpers/my_custom_form_builder.rb
@@ -1,5 +1,5 @@
 class MyCustomFormBuilder < ActionView::Helpers::FormBuilder
-	def error_messages
-		'' # return empty string
-	end
+  def error_messages
+    '' # return empty string
+  end
 end
--- spec/lib/bootstrap_forms/bootstrap_form_for_spec.rb
+++ spec/lib/bootstrap_forms/bootstrap_form_for_spec.rb
@@ -1,35 +1,35 @@
 require 'spec_helper'
 
 describe 'bootstrap_form_for' do
-	describe 'default_form_builder' do
-		it 'should be accessible' do
-			BootstrapForms.should respond_to(:default_form_builder)
-		end
+  describe 'default_form_builder' do
+    it 'should be accessible' do
+      BootstrapForms.should respond_to(:default_form_builder)
+    end
 
-		it 'should be the BootstrapForms form_builder by default' do
-			BootstrapForms.default_form_builder.should == BootstrapForms::FormBuilder
-		end
+    it 'should be the BootstrapForms form_builder by default' do
+      BootstrapForms.default_form_builder.should == BootstrapForms::FormBuilder
+    end
 
-		context 'when set to something else' do
-			before do
-				BootstrapForms.default_form_builder = MyCustomFormBuilder
-			end
+    context 'when set to something else' do
+      before do
+        BootstrapForms.default_form_builder = MyCustomFormBuilder
+      end
 
-			it 'should be that other thing' do
-				BootstrapForms.default_form_builder.should == MyCustomFormBuilder
-			end
+      it 'should be that other thing' do
+        BootstrapForms.default_form_builder.should == MyCustomFormBuilder
+      end
 
-			describe 'projects/new.html.erb', :type => :view do
-				before do
-					assign :project, Project.new
-					render :file => 'projects/new', :layout => 'layouts/application', :handlers => [:erb]
-				end
+      describe 'projects/new.html.erb', :type => :view do
+        before do
+          assign :project, Project.new
+          render :file => 'projects/new', :layout => 'layouts/application', :handlers => [:erb]
+        end
 
-				it 'should render with the other form builder' do
-					# in other words, it shouldn't be wrapped with the bootstrap stuff
-					rendered.should_not match /<div class=\"control-group\"><label class=\"control-label\" for=\"item_name\">Name<\/label><div class=\"controls\">.*<\/div><\/div>/
-				end
-			end
-		end
-	end
+        it 'should render with the other form builder' do
+          # in other words, it shouldn't be wrapped with the bootstrap stuff
+          rendered.should_not match /<div class=\"control-group\"><label class=\"control-label\" for=\"item_name\">Name<\/label><div class=\"controls\">.*<\/div><\/div>/
+        end
+      end
+    end
+  end
 end
