fc2af3a3b646e762219028270d9928d993b77e00
Christophe Maximin <christophe.maximin@gmail.com>
2013-03-30 23:34:13 +0000

    Added boolean option :summary_errors to bootstrap_form_for

--- lib/bootstrap_forms/helpers/form_helper.rb
+++ lib/bootstrap_forms/helpers/form_helper.rb
@@ -4,8 +4,12 @@ module BootstrapForms
       def bootstrap_form_for(record, options = {}, &block)
         options[:builder] ||= BootstrapForms.default_form_builder
 
-        form_for(record, options) do |f|
-          if f.object.respond_to?(:errors)
+        form_options = options.deep_dup
+        options[:summary_errors] = true unless form_options.has_key?(:summary_errors)
+        form_options.delete(:summary_errors)
+
+        form_for(record, form_options) do |f|
+          if f.object.respond_to?(:errors) and options[:summary_errors]
             f.error_messages.html_safe + capture(f, &block).html_safe
           else
             capture(f, &block).html_safe
